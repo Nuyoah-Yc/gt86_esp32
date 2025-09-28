8b3d86123a7d771f9cd172700c2718b438f2d3ce
youchuang <youchuang@vmos.cn>
2025-09-28 13:50:53 +0800

    添加涡轮图标

--- src/gt86clock.cpp
+++ src/gt86clock.cpp
@@ -1,6 +1,6 @@
 #include "gt86clock.h"
 
-U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE, /* clock=*/22, /* data=*/21);
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE, /* clock=*/22, /* data=*/23);
 RTC_DS3231 RTC;
 RTC_DS3231 Clock;
 DateTime dateTime;
